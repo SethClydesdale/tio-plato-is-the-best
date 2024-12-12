0b229b498f846e8629620cd747a4827af54e7b1a
Seth Clydesdale <sethc1995@gmail.com>
2024-12-12 13:05:28 -0500

    add analytics

---
+++ aeon-system/ga.js
@@ -0,0 +1,17 @@
+// Google Analytics
+// disabled if used locally or on another host
+if (window.location.hostname == 'sethclydesdale.github.io') {
+  (function (window, document, id) {
+    var gtag = document.createElement('SCRIPT');
+    gtag.async = true;
+    gtag.src = 'https://www.googletagmanager.com/gtag/js?id=' + id;
+    gtag.onload = function () {
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', id);
+    };
+    
+    document.head.appendChild(gtag);
+  }(window, document, 'G-77G2R7C75J'));
+}
